8537d5f87e4c0bcb9ce08f59e4fabf5ada079b58
Anar Enhsaihan <anar.enhsaihan@cittacorp.com>
2015-07-09 17:11:16 -0400

    Add text fields for naming a collection and providing it a description

--- Cliq/Cliq/CliqNameCollectionViewController.swift
+++ Cliq/Cliq/CliqNameCollectionViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class CliqNameCollectionViewController: UIViewController {
     
     @IBOutlet weak var coverPhotoImageView: UIImageView!
+    @IBOutlet weak var nameCollectionTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
     var imageForCoverPhoto : UIImage!
     
     override func viewDidLoad() {
@@ -18,5 +21,9 @@ class CliqNameCollectionViewController: UIViewController {
         coverPhotoImageView.image = imageForCoverPhoto
         
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "CREATE", style: UIBarButtonItemStyle.Plain, target: self, action: nil)
+        
+        // make placeholder text color white
+        nameCollectionTextField.attributedPlaceholder = NSAttributedString(string: "Name your collection", attributes: [NSForegroundColorAttributeName : UIColor.whiteColor()])
+        descriptionTextField.attributedPlaceholder = NSAttributedString(string: "Give a short description if you like", attributes: [NSForegroundColorAttributeName : UIColor.whiteColor()])
     }
 }
