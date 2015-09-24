92c16f69443b4e2d5608e58af993f7e7e07174f9
Anar Enhsaihan <anar.enhsaihan@cittacorp.com>
2015-09-23 23:28:05 -0400

    Photos all upload with a pointer to the cliq album they belong to.

--- Cliq/Cliq/CliqNameCollectionViewController.swift
+++ Cliq/Cliq/CliqNameCollectionViewController.swift
@@ -27,6 +27,8 @@ class CliqNameCollectionViewController: UIViewController {
     
     var userPhotos = [PFObject]()
     
+    var activityIndicator : UIActivityIndicatorView?
+    
     override func viewDidLoad() {
         
         coverPhotoImageView.image = imageForCoverPhoto
@@ -38,43 +40,21 @@ class CliqNameCollectionViewController: UIViewController {
         descriptionTextField.attributedPlaceholder = NSAttributedString(string: "Give a short description if you like", attributes: [NSForegroundColorAttributeName : UIColor.whiteColor()])
     }
     
-    func uploadCollection() {
+    func uploadPhotos() {
         
-        // prevent user from interacting with the app during creation of the cliq
-        UIApplication.sharedApplication().beginIgnoringInteractionEvents()
-        
-        // superimpose a spinning indicator while the cliq is being created
-        var activityIndicator : UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50)) as UIActivityIndicatorView
-        activityIndicator.center = self.view.center
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.WhiteLarge
-        view.addSubview(activityIndicator)
-        activityIndicator.startAnimating()
-        
-        // TODO: once cliq is created, revert user to home view controller
-        
-        var cliq = self.cliqGroup!
-        cliq["collectionName"] = nameCollectionTextField.text
-        cliq["description"] = descriptionTextField.text
-        cliq["private"] = privateCollectionSwitch.on
-        cliq["customDateTime"] = customDateTimeSwitch.on
-        cliq["inviteFriends"] = inviteFriendsAtLocationSwitch.on
-        cliq.saveInBackground()
-        
-        // [Anar] Upload photo and associated data to Parse
-        
-        var userPhoto : PFObject
-        
-        if let image = imageForCoverPhoto {
-            let imageData = UIImageJPEGRepresentation(image, 0.55)
-            let imageFile = PFFile(name: "image.jpeg", data: imageData)
-            userPhoto = PFObject(className: "UserPhoto")
+        for userPhoto in userPhotos {
+            
+//            let imageData = UIImageJPEGRepresentation(image, 0.55)
+//            let imageFile = PFFile(name: "image.jpeg", data: imageData)
             
             let user = PFUser.currentUser()! as PFObject
             
+            
+            // TODO: [Anar] give each photo the cliqAlbum to which it belongs
+            var cliq = self.cliqGroup!
+            
             userPhoto["creator"] = PFUser.currentUser()
             userPhoto["cliqGroup"] = cliq // [Anar] Pointer to the cliqGroup to which the photo belongs
-            userPhoto["imageFile"] = imageFile // [Anar] Save as binary on Parse
             userPhoto.saveInBackgroundWithBlock({ (success, error) -> Void in
                 if (success) {
                     println("Saved photo successfully")
@@ -94,9 +74,11 @@ class CliqNameCollectionViewController: UIViewController {
                                 var listPhotosVC : CliqListPhotosViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("ListPhotosVC") as! CliqListPhotosViewController
                                 listPhotosVC.cliqId = cliq.objectId!
                                 
-                                let navController : UINavigationController = self.navigationController!
-                                navController.popToRootViewControllerAnimated(false)
-                                navController.pushViewController(listPhotosVC, animated: false) // [Anar] play around with true/false to your liking
+                                if let navController : UINavigationController = self.navigationController {
+                                    navController.popToRootViewControllerAnimated(false)
+                                    navController.pushViewController(listPhotosVC, animated: false) // [Anar] play around with true/false to your liking
+                                }
+                                
                                 
                             } else {
                                 
@@ -106,7 +88,7 @@ class CliqNameCollectionViewController: UIViewController {
                             }
                             
                             UIApplication.sharedApplication().endIgnoringInteractionEvents()
-                            activityIndicator.stopAnimating()
+                            self.activityIndicator!.stopAnimating()
                         })
                         
                     })
@@ -119,12 +101,102 @@ class CliqNameCollectionViewController: UIViewController {
                     // [Anar] allow user inputs again
                     dispatch_async(dispatch_get_main_queue(), { () -> Void in
                         UIApplication.sharedApplication().endIgnoringInteractionEvents()
-                        activityIndicator.stopAnimating()
+                        self.activityIndicator!.stopAnimating()
                     })
                 }
             })
-            
         }
+    }
+    
+    func uploadCollection() {
+        
+        // prevent user from interacting with the app during creation of the cliq
+        UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+        
+        // superimpose a spinning indicator while the cliq is being created
+        activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50)) as UIActivityIndicatorView
+        activityIndicator!.center = self.view.center
+        activityIndicator!.hidesWhenStopped = true
+        activityIndicator!.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.WhiteLarge
+        view.addSubview(activityIndicator!)
+        activityIndicator!.startAnimating()
+        
+        // TODO: once cliq is created, revert user to home view controller
+        
+        var cliq = self.cliqGroup!
+        cliq["collectionName"] = nameCollectionTextField.text
+        cliq["description"] = descriptionTextField.text
+        cliq["private"] = privateCollectionSwitch.on
+        cliq["customDateTime"] = customDateTimeSwitch.on
+        cliq["inviteFriends"] = inviteFriendsAtLocationSwitch.on
+        cliq.saveInBackground()
+        
+        uploadPhotos()
+        
+        // [Anar] Upload photo and associated data to Parse
+        
+//        var userPhoto : PFObject
+//        
+//        if let image = imageForCoverPhoto {
+//            let imageData = UIImageJPEGRepresentation(image, 0.55)
+//            let imageFile = PFFile(name: "image.jpeg", data: imageData)
+//            userPhoto = PFObject(className: "UserPhoto")
+//            
+//            let user = PFUser.currentUser()! as PFObject
+//            
+//            userPhoto["creator"] = PFUser.currentUser()
+//            userPhoto["cliqGroup"] = cliq // [Anar] Pointer to the cliqGroup to which the photo belongs
+//            userPhoto["imageFile"] = imageFile // [Anar] Save as binary on Parse
+//            userPhoto.saveInBackgroundWithBlock({ (success, error) -> Void in
+//                if (success) {
+//                    println("Saved photo successfully")
+//                    
+//                    cliq["facebookId"] = user["facebookId"]
+//                    cliq["coverPhoto"] = userPhoto // [Anar] Pointer to the photo that was just created
+//                    cliq.saveInBackgroundWithBlock({ (success, error) -> Void in
+//                        
+//                        // [Anar] allow user inputs again
+//                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                            
+//                            if (success) {
+//                                println("Saved cliq successfully")
+//                                
+//                                // [Anar] pop to home, and then advance to list photos VC
+//                                
+//                                var listPhotosVC : CliqListPhotosViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("ListPhotosVC") as! CliqListPhotosViewController
+//                                listPhotosVC.cliqId = cliq.objectId!
+//                                
+//                                let navController : UINavigationController = self.navigationController!
+//                                navController.popToRootViewControllerAnimated(false)
+//                                navController.pushViewController(listPhotosVC, animated: false) // [Anar] play around with true/false to your liking
+//                                
+//                            } else {
+//                                
+//                                // TODO: [Anar] might be nice to let the user know with an alert controller
+//                                
+//                                println(error)
+//                            }
+//                            
+//                            UIApplication.sharedApplication().endIgnoringInteractionEvents()
+//                            activityIndicator.stopAnimating()
+//                        })
+//                        
+//                    })
+//                } else {
+//                    
+//                    // TODO: [Anar] might be a good idea to let user know with an alert controller
+//                    
+//                    println(error)
+//                    
+//                    // [Anar] allow user inputs again
+//                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                        UIApplication.sharedApplication().endIgnoringInteractionEvents()
+//                        activityIndicator.stopAnimating()
+//                    })
+//                }
+//            })
+//            
+//        }
         
     }
     
